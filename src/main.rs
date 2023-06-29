39469174a2c506df85de29b3e4f7bf686b4e04c0
benpospo@gmail.com <93595333+taraxsmallboy@users.noreply.github.com>
2023-06-29 22:42:36 +0200

    WE HAVE KEYBINDS LETS GO!

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod glsl_reader;
 use glium::{glutin, Surface};
 
 fn main() {
+
     // event loop creation
     let event_loop = glutin::event_loop::EventLoop::new();
     // the window
@@ -46,7 +47,7 @@ fn main() {
         position: [-0.5, -0.0],
     };
     
-    let shape = vec![vertex1, vertex2, vertex3, vertex4, vertex5, vertex6];
+    let mut shape = vec![vertex1, vertex2, vertex3, vertex4, vertex5, vertex6];
     // Vertex handler
     let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
     let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
@@ -61,38 +62,74 @@ fn main() {
         glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None)
             .unwrap();
 
-    event_loop.run(move |event, _, control_flow| {
-        let next_frame_time =
-            std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
-        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
-
-        match event {
-            glutin::event::Event::WindowEvent { event, .. } => match event {
-                glutin::event::WindowEvent::CloseRequested => {
-                    *control_flow = glutin::event_loop::ControlFlow::Exit;
-                    return;
-                }
+        event_loop.run(move |event, _, control_flow| {
+            let next_frame_time =
+                std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
+            *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+        
+            match event {
+                glutin::event::Event::WindowEvent { event, .. } => match event {
+                    glutin::event::WindowEvent::CloseRequested => {
+                        *control_flow = glutin::event_loop::ControlFlow::Exit;
+                        return;
+                    }
+                    glutin::event::WindowEvent::KeyboardInput { input, .. } => {
+                        if let Some(keycode) = input.virtual_keycode {
+                            match keycode {
+                                glutin::event::VirtualKeyCode::Up => {
+                                    // Update the vertex positions
+                                    for vertex in &mut shape {
+                                        vertex.position[1] += 0.01;  // Update Y coordinate
+                                    }
+                                }
+                                glutin::event::VirtualKeyCode::Down => {
+                                    // Update the vertex positions
+                                    for vertex in &mut shape {
+                                        vertex.position[1] -= 0.01;  // Update Y coordinate
+                                    }
+                                }
+                                glutin::event::VirtualKeyCode::Left => {
+                                    // Update the vertex positions
+                                    for vertex in &mut shape {
+                                        vertex.position[0] -= 0.01;  // Update X coordinate
+                                    }
+                                }
+                                glutin::event::VirtualKeyCode::Right => {
+                                    // Update the vertex positions
+                                    for vertex in &mut shape {
+                                        vertex.position[0] += 0.01;  // Update X coordinate
+                                    }
+                                }
+                                _ => (),
+                            }
+                        }
+                    }
+                    _ => (),
+                },
+                glutin::event::Event::NewEvents(cause) => match cause {
+                    glutin::event::StartCause::ResumeTimeReached { .. } => (),
+                    glutin::event::StartCause::Init => (),
+                    _ => return,
+                },
                 _ => return,
-            },
-            glutin::event::Event::NewEvents(cause) => match cause {
-                glutin::event::StartCause::ResumeTimeReached { .. } => (),
-                glutin::event::StartCause::Init => (),
-                _ => return,
-            },
-            _ => return,
-        }
-
-        let mut target = display.draw();
-        target.clear_color(0.0, 1.0, 1.0, 1.0);
-        target
-            .draw(
-                &vertex_buffer,
-                &indices,
-                &program,
-                &glium::uniforms::EmptyUniforms,
-                &Default::default(),
-            )
-            .unwrap();
-        target.finish().unwrap();
-    });
+            }
+        
+        
+            // Update the vertex buffer with the modified positions
+            vertex_buffer.write(&shape);
+        
+            let mut target = display.draw();
+            target.clear_color(0.0, 1.0, 1.0, 1.0);
+            target
+                .draw(
+                    &vertex_buffer,
+                    &indices,
+                    &program,
+                    &glium::uniforms::EmptyUniforms,
+                    &Default::default(),
+                )
+                .unwrap();
+            target.finish().unwrap();
+        });
+            
 }
