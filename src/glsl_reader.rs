7e3fd9123a15fd495ce8cba1ce3feceac36c1c3b
benpospo@gmail.com <93595333+taraxsmallboy@users.noreply.github.com>
2023-06-30 22:25:27 +0200

    started working on textures

--- src/glsl_reader.rs
+++ src/glsl_reader.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::Read;
 
 pub fn read(file_name: &str) -> String {
-    let shader_path = "src/resources/shader";
+    let shader_path = "src/resources/shaders";
     let full_path = format!("{}/{}", shader_path, file_name);
     let mut file = File::open(full_path).expect("Failed to open file");
     let mut file_src = String::new();
