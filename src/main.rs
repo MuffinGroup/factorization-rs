8c9296bff0ef3c1bb87c234efc0842dd81d620cd
benpospo@gmail.com <93595333+taraxsmallboy@users.noreply.github.com>
2023-07-18 19:27:51 +0200

    added camera

--- src/main.rs
+++ src/main.rs
@@ -14,26 +14,32 @@ fn main() {
 
     let positions = glium::VertexBuffer::new(&display, &teapot::VERTICES).unwrap();
     let normals = glium::VertexBuffer::new(&display, &teapot::NORMALS).unwrap();
-    let indices = glium::IndexBuffer::new(&display, glium::index::PrimitiveType::TrianglesList,
-                                          &teapot::INDICES).unwrap();
+    let indices = glium::IndexBuffer::new(
+        &display,
+        glium::index::PrimitiveType::TrianglesList,
+        &teapot::INDICES,
+    )
+    .unwrap();
 
     let mut zoom = 2.0;
 
     let vertex_shader_src = r#"
-        #version 150
+    #version 140
 
-        in vec3 position;
-        in vec3 normal;
-
-        out vec3 v_normal;
-
-        uniform mat4 perspective;
-        uniform mat4 matrix;
-
-        void main() {
-            v_normal = transpose(inverse(mat3(matrix))) * normal;
-            gl_Position = perspective * matrix * vec4(position, 1.0);
-        }
+    in vec3 position;
+    in vec3 normal;
+    
+    out vec3 v_normal;
+    
+    uniform mat4 perspective;
+    uniform mat4 view;
+    uniform mat4 model;
+    
+    void main() {
+        mat4 modelview = view * model;
+        v_normal = transpose(inverse(mat3(modelview))) * normal;
+        gl_Position = perspective * modelview * vec4(position, 1.0);
+    }
     "#;
 
     let fragment_shader_src = r#"
@@ -51,12 +57,13 @@ fn main() {
         }
     "#;
 
-    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src,
-                                              None).unwrap();
+    let program =
+        glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None)
+            .unwrap();
 
     event_loop.run(move |event, _, control_flow| {
-        let next_frame_time = std::time::Instant::now() +
-            std::time::Duration::from_nanos(16_666_667);
+        let next_frame_time =
+            std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
         *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
 
         match event {
@@ -64,7 +71,10 @@ fn main() {
                 glutin::event::WindowEvent::CloseRequested => {
                     *control_flow = glutin::event_loop::ControlFlow::Exit;
                     return;
-                },
+                }
+                glutin::event::WindowEvent::Resized(window_size) => {
+                    display.gl_window().resize(window_size.into());
+                }
                 glutin::event::WindowEvent::KeyboardInput { input, .. } => {
                     if let Some(keycode) = input.virtual_keycode {
                         match keycode {
@@ -80,6 +90,59 @@ fn main() {
                 }
                 _ => return,
             },
+            glutin::event::Event::RedrawRequested(_) => {
+                let mut target = display.draw();
+                target.clear_color_and_depth((0.0, 0.0, 1.0, 1.0), 1.0);
+        
+                let model = [
+                    [0.01, 0.0, 0.0, 0.0],
+                    [0.0, 0.01, 0.0, 0.0],
+                    [0.0, 0.0, 0.01, 0.0],
+                    [0.0, 0.0, zoom, 1.0f32],
+                ];
+        
+                let perspective = {
+                    let (width, height) = target.get_dimensions();
+                    let aspect_ratio = height as f32 / width as f32;
+        
+                    let fov: f32 = 3.141592 / 3.0;
+                    let zfar = 1024.0;
+                    let znear = 0.1;
+        
+                    let f = 1.0 / (fov / 2.0).tan();
+        
+                    [
+                        [f * aspect_ratio, 0.0, 0.0, 0.0],
+                        [0.0, f, 0.0, 0.0],
+                        [0.0, 0.0, (zfar + znear) / (zfar - znear), 1.0],
+                        [0.0, 0.0, -(2.0 * zfar * znear) / (zfar - znear), 0.0],
+                    ]
+                };
+                let light = [-1.0, 0.4, 0.9f32];
+
+                let params = glium::DrawParameters {
+                    depth: glium::Depth {
+                        test: glium::DepthTest::IfLess,
+                        write: true,
+                        ..Default::default()
+                    },
+                    backface_culling: glium::draw_parameters::BackfaceCullingMode::CullClockwise,
+                    ..Default::default()
+                };
+        
+                let view = view_matrix(&[2.0, -1.0, 1.0], &[-2.0, 1.0, 1.0], &[0.0, 1.0, 0.0]);
+        
+                target
+                    .draw(
+                        (&positions, &normals),
+                        &indices,
+                        &program,
+                        &uniform! { model: model, view: view, perspective: perspective, u_light: light },
+                        &params,
+                    )
+                    .unwrap();
+                target.finish().unwrap();
+            },
             glutin::event::Event::NewEvents(cause) => match cause {
                 glutin::event::StartCause::ResumeTimeReached { .. } => (),
                 glutin::event::StartCause::Init => (),
@@ -88,48 +151,44 @@ fn main() {
             _ => return,
         }
 
-        let mut target = display.draw();
-        target.clear_color_and_depth((0.0, 0.0, 1.0, 1.0), 1.0);
 
-        let matrix = [
-            [0.01, 0.0, 0.0, 0.0],
-            [0.0, 0.01, 0.0, 0.0],
-            [0.0, 0.0, 0.01, 0.0],
-            [0.0, 0.0, zoom, 1.0f32]
-        ];
+        fn view_matrix(position: &[f32; 3], direction: &[f32; 3], up: &[f32; 3]) -> [[f32; 4]; 4] {
+            let f = {
+                let f = direction;
+                let len = f[0] * f[0] + f[1] * f[1] + f[2] * f[2];
+                let len = len.sqrt();
+                [f[0] / len, f[1] / len, f[2] / len]
+            };
 
-        let perspective = {
-            let (width, height) = target.get_dimensions();
-            let aspect_ratio = height as f32 / width as f32;
+            let s = [
+                up[1] * f[2] - up[2] * f[1],
+                up[2] * f[0] - up[0] * f[2],
+                up[0] * f[1] - up[1] * f[0],
+            ];
 
-            let fov: f32 = 3.141592 / 3.0;
-            let zfar = 1024.0;
-            let znear = 0.1;
+            let s_norm = {
+                let len = s[0] * s[0] + s[1] * s[1] + s[2] * s[2];
+                let len = len.sqrt();
+                [s[0] / len, s[1] / len, s[2] / len]
+            };
 
-            let f = 1.0 / (fov / 2.0).tan();
+            let u = [
+                f[1] * s_norm[2] - f[2] * s_norm[1],
+                f[2] * s_norm[0] - f[0] * s_norm[2],
+                f[0] * s_norm[1] - f[1] * s_norm[0],
+            ];
+
+            let p = [
+                -position[0] * s_norm[0] - position[1] * s_norm[1] - position[2] * s_norm[2],
+                -position[0] * u[0] - position[1] * u[1] - position[2] * u[2],
+                -position[0] * f[0] - position[1] * f[1] - position[2] * f[2],
+            ];
 
             [
-                [f *   aspect_ratio   ,    0.0,              0.0              ,   0.0],
-                [         0.0         ,     f ,              0.0              ,   0.0],
-                [         0.0         ,    0.0,  (zfar+znear)/(zfar-znear)    ,   1.0],
-                [         0.0         ,    0.0, -(2.0*zfar*znear)/(zfar-znear),   0.0],
+                [s_norm[0], u[0], f[0], 0.0],
+                [s_norm[1], u[1], f[1], 0.0],
+                [s_norm[2], u[2], f[2], 0.0],
+                [p[0], p[1], p[2], 1.0],
             ]
-        };
-
-        let light = [-1.0, 0.4, 0.9f32];
-
-        let params = glium::DrawParameters {
-            depth: glium::Depth {
-                test: glium::draw_parameters::DepthTest::IfLess,
-                write: true,
-                .. Default::default()
-            },
-            .. Default::default()
-        };
-
-        target.draw((&positions, &normals), &indices, &program,
-                    &uniform! { matrix: matrix, perspective: perspective, u_light: light },
-                    &params).unwrap();
-        target.finish().unwrap();
-    });
+        }});
 }
