a0fa59b61f8e0ad3146adbcd1c533100fde1a146
benpospo@gmail.com <93595333+taraxsmallboy@users.noreply.github.com>
2023-07-18 22:20:26 +0200

    implement zoom

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ fn main() {
     )
     .unwrap();
 
-    let mut zoom = 2.0;
+    let mut zoom = 10.0;
 
     let vertex_shader_src = r#"
     #version 140
@@ -72,9 +72,6 @@ fn main() {
                     *control_flow = glutin::event_loop::ControlFlow::Exit;
                     return;
                 }
-                glutin::event::WindowEvent::Resized(window_size) => {
-                    display.gl_window().resize(window_size.into());
-                }
                 glutin::event::WindowEvent::KeyboardInput { input, .. } => {
                     if let Some(keycode) = input.virtual_keycode {
                         match keycode {
@@ -90,59 +87,6 @@ fn main() {
                 }
                 _ => return,
             },
-            glutin::event::Event::RedrawRequested(_) => {
-                let mut target = display.draw();
-                target.clear_color_and_depth((0.0, 0.0, 1.0, 1.0), 1.0);
-        
-                let model = [
-                    [0.01, 0.0, 0.0, 0.0],
-                    [0.0, 0.01, 0.0, 0.0],
-                    [0.0, 0.0, 0.01, 0.0],
-                    [0.0, 0.0, zoom, 1.0f32],
-                ];
-        
-                let perspective = {
-                    let (width, height) = target.get_dimensions();
-                    let aspect_ratio = height as f32 / width as f32;
-        
-                    let fov: f32 = 3.141592 / 3.0;
-                    let zfar = 1024.0;
-                    let znear = 0.1;
-        
-                    let f = 1.0 / (fov / 2.0).tan();
-        
-                    [
-                        [f * aspect_ratio, 0.0, 0.0, 0.0],
-                        [0.0, f, 0.0, 0.0],
-                        [0.0, 0.0, (zfar + znear) / (zfar - znear), 1.0],
-                        [0.0, 0.0, -(2.0 * zfar * znear) / (zfar - znear), 0.0],
-                    ]
-                };
-                let light = [-1.0, 0.4, 0.9f32];
-
-                let params = glium::DrawParameters {
-                    depth: glium::Depth {
-                        test: glium::DepthTest::IfLess,
-                        write: true,
-                        ..Default::default()
-                    },
-                    backface_culling: glium::draw_parameters::BackfaceCullingMode::CullClockwise,
-                    ..Default::default()
-                };
-        
-                let view = view_matrix(&[2.0, -1.0, 1.0], &[-2.0, 1.0, 1.0], &[0.0, 1.0, 0.0]);
-        
-                target
-                    .draw(
-                        (&positions, &normals),
-                        &indices,
-                        &program,
-                        &uniform! { model: model, view: view, perspective: perspective, u_light: light },
-                        &params,
-                    )
-                    .unwrap();
-                target.finish().unwrap();
-            },
             glutin::event::Event::NewEvents(cause) => match cause {
                 glutin::event::StartCause::ResumeTimeReached { .. } => (),
                 glutin::event::StartCause::Init => (),
@@ -151,44 +95,103 @@ fn main() {
             _ => return,
         }
 
+        let mut target = display.draw();
+        target.clear_color_and_depth((0.0, 0.0, 1.0, 1.0), 1.0);
 
-        fn view_matrix(position: &[f32; 3], direction: &[f32; 3], up: &[f32; 3]) -> [[f32; 4]; 4] {
-            let f = {
-                let f = direction;
-                let len = f[0] * f[0] + f[1] * f[1] + f[2] * f[2];
-                let len = len.sqrt();
-                [f[0] / len, f[1] / len, f[2] / len]
-            };
+        let model = [
+            [0.01, 0.0, 0.0, 0.0],
+            [0.0, 0.01, 0.0, 0.0],
+            [0.0, 0.0, 0.01, 0.0],
+            [0.0, 0.0, 2.0, 1.0f32],
+        ];
 
-            let s = [
-                up[1] * f[2] - up[2] * f[1],
-                up[2] * f[0] - up[0] * f[2],
-                up[0] * f[1] - up[1] * f[0],
-            ];
+        let perspective = {
+            let (width, height) = target.get_dimensions();
+            let aspect_ratio = height as f32 / width as f32;
 
-            let s_norm = {
-                let len = s[0] * s[0] + s[1] * s[1] + s[2] * s[2];
-                let len = len.sqrt();
-                [s[0] / len, s[1] / len, s[2] / len]
-            };
+            let fov: f32 = 3.141592 / 3.0;
+            let zfar = 1024.0;
+            let znear = 0.1;
 
-            let u = [
-                f[1] * s_norm[2] - f[2] * s_norm[1],
-                f[2] * s_norm[0] - f[0] * s_norm[2],
-                f[0] * s_norm[1] - f[1] * s_norm[0],
-            ];
-
-            let p = [
-                -position[0] * s_norm[0] - position[1] * s_norm[1] - position[2] * s_norm[2],
-                -position[0] * u[0] - position[1] * u[1] - position[2] * u[2],
-                -position[0] * f[0] - position[1] * f[1] - position[2] * f[2],
-            ];
+            let f = 1.0 / (fov / 2.0).tan();
 
             [
-                [s_norm[0], u[0], f[0], 0.0],
-                [s_norm[1], u[1], f[1], 0.0],
-                [s_norm[2], u[2], f[2], 0.0],
-                [p[0], p[1], p[2], 1.0],
+                [f * aspect_ratio, 0.0, 0.0, 0.0],
+                [0.0, f, 0.0, 0.0],
+                [0.0, 0.0, (zfar + znear) / (zfar - znear), 1.0],
+                [0.0, 0.0, -(2.0 * zfar * znear) / (zfar - znear), 0.0],
             ]
-        }});
+        };
+        let light = [-1.0, 0.4, 0.9f32];
+
+        let params = glium::DrawParameters {
+            depth: glium::Depth {
+                test: glium::DepthTest::IfLess,
+                write: true,
+                ..Default::default()
+            },
+            backface_culling: glium::draw_parameters::BackfaceCullingMode::CullClockwise,
+            ..Default::default()
+        };
+
+        let view = view_matrix(&[2.0, -1.0, 1.0], &[-2.0, 1.0, 1.0], &[0.0, 1.0, 0.0], zoom);
+
+        target
+            .draw(
+                (&positions, &normals),
+                &indices,
+                &program,
+                &uniform! { model: model, view: view, perspective: perspective, u_light: light },
+                &params,
+            )
+            .unwrap();
+        target.finish().unwrap();
+    });
+}
+
+fn view_matrix(position: &[f32; 3], direction: &[f32; 3], up: &[f32; 3], zoom_factor: f32) -> [[f32; 4]; 4] {
+    let f = {
+        let f = direction;
+        let len = f[0] * f[0] + f[1] * f[1] + f[2] * f[2];
+        let len = len.sqrt();
+        [f[0] / len, f[1] / len, f[2] / len]
+    };
+
+    // Calculate the normalized side vector (right)
+    let s = [
+        up[1] * f[2] - up[2] * f[1],
+        up[2] * f[0] - up[0] * f[2],
+        up[0] * f[1] - up[1] * f[0],
+    ];
+
+    // Calculate the normalized up vector
+    let s_norm = {
+        let len = s[0] * s[0] + s[1] * s[1] + s[2] * s[2];
+        let len = len.sqrt();
+        [s[0] / len, s[1] / len, s[2] / len]
+    };
+
+    // Calculate the final up vector
+    let u = [
+        f[1] * s_norm[2] - f[2] * s_norm[1],
+        f[2] * s_norm[0] - f[0] * s_norm[2],
+        f[0] * s_norm[1] - f[1] * s_norm[0],
+    ];
+
+    // Calculate the final position with zoom applied
+    let p = [
+        position[0] + f[0] * zoom_factor,
+        position[1] + f[1] * zoom_factor,
+        position[2] + f[2] * zoom_factor,
+    ];
+
+    // Construct the view matrix
+    [
+        [s_norm[0], u[0], -f[0], 0.0],
+        [s_norm[1], u[1], -f[1], 0.0],
+        [s_norm[2], u[2], -f[2], 0.0],
+        [-s_norm[0] * p[0] - s_norm[1] * p[1] - s_norm[2] * p[2],
+         -u[0] * p[0] - u[1] * p[1] - u[2] * p[2],
+         f[0] * p[0] + f[1] * p[1] + f[2] * p[2], 1.0],
+    ]
 }
