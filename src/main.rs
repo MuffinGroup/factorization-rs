ccbf98859d75827b964689befbba660e88874468
thepigcat76 <benpospo@gmail.com>
2023-07-22 14:27:24 +0200

    test

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,7 @@
 #[macro_use]
-#[allow(macro_use_extern_crate)]
 extern crate glium;
 
 mod teapot;
-#[macro_use]
-mod logger;
-mod util;
-mod colors;
-
-use util::*;
 
 fn main() {
     #[allow(unused_imports)]
@@ -21,30 +14,49 @@ fn main() {
 
     let positions = glium::VertexBuffer::new(&display, &teapot::VERTICES).unwrap();
     let normals = glium::VertexBuffer::new(&display, &teapot::NORMALS).unwrap();
-    let indices = glium::IndexBuffer::new(
-        &display,
-        glium::index::PrimitiveType::TrianglesList,
-        &teapot::INDICES,
-    )
-    .unwrap();
+    let indices = glium::IndexBuffer::new(&display, glium::index::PrimitiveType::TrianglesList,
+                                          &teapot::INDICES).unwrap();
 
-    let mut zoom = 10.0;
+    let mut zoom = 2.0;
 
-    let vertex_shader_src = include_str!("../resources/shaders/vertex_shader.vert");
-    let vertex_shader = &load_shader(vertex_shader_src, "vertex_shader.vert");
+    let vertex_shader_src = r#"
+        #version 150
 
-    let fragment_shader_src = include_str!("../resources/shaders/fragment_shader.frag");
-    let fragment_shader = &load_shader(fragment_shader_src, "fragment_shader.frag");
+        in vec3 position;
+        in vec3 normal;
 
-    let program =
-        glium::Program::from_source(&display, vertex_shader, fragment_shader, None)
-            .unwrap();
+        out vec3 v_normal;
 
-    log!("Started sucessful");
+        uniform mat4 perspective;
+        uniform mat4 matrix;
+
+        void main() {
+            v_normal = transpose(inverse(mat3(matrix))) * normal;
+            gl_Position = perspective * matrix * vec4(position, 1.0);
+        }
+    "#;
+
+    let fragment_shader_src = r#"
+        #version 150
+
+        in vec3 v_normal;
+        out vec4 color;
+        uniform vec3 u_light;
+
+        void main() {
+            float brightness = dot(normalize(v_normal), normalize(u_light));
+            vec3 dark_color = vec3(0.6, 0.0, 0.0);
+            vec3 regular_color = vec3(1.0, 0.0, 0.0);
+            color = vec4(mix(dark_color, regular_color, brightness), 1.0);
+        }
+    "#;
+
+    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src,
+                                              None).unwrap();
 
     event_loop.run(move |event, _, control_flow| {
-        let next_frame_time =
-            std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
+        let next_frame_time = std::time::Instant::now() +
+            std::time::Duration::from_nanos(16_666_667);
         *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
 
         match event {
@@ -52,7 +64,7 @@ fn main() {
                 glutin::event::WindowEvent::CloseRequested => {
                     *control_flow = glutin::event_loop::ControlFlow::Exit;
                     return;
-                }
+                },
                 glutin::event::WindowEvent::KeyboardInput { input, .. } => {
                     if let Some(keycode) = input.virtual_keycode {
                         match keycode {
@@ -79,11 +91,11 @@ fn main() {
         let mut target = display.draw();
         target.clear_color_and_depth((0.0, 0.0, 1.0, 1.0), 1.0);
 
-        let model = [
+        let mut matrix = [
             [0.01, 0.0, 0.0, 0.0],
             [0.0, 0.01, 0.0, 0.0],
             [0.0, 0.0, 0.01, 0.0],
-            [0.0, 0.0, 2.0, 1.0f32],
+            [0.0, 0.0, zoom, 1.0f32]
         ];
 
         let perspective = {
@@ -97,82 +109,27 @@ fn main() {
             let f = 1.0 / (fov / 2.0).tan();
 
             [
-                [f * aspect_ratio, 0.0, 0.0, 0.0],
-                [0.0, f, 0.0, 0.0],
-                [0.0, 0.0, (zfar + znear) / (zfar - znear), 1.0],
-                [0.0, 0.0, -(2.0 * zfar * znear) / (zfar - znear), 0.0],
+                [f *   aspect_ratio   ,    0.0,              0.0              ,   0.0],
+                [         0.0         ,     f ,              0.0              ,   0.0],
+                [         0.0         ,    0.0,  (zfar+znear)/(zfar-znear)    ,   1.0],
+                [         0.0         ,    0.0, -(2.0*zfar*znear)/(zfar-znear),   0.0],
             ]
         };
+
         let light = [-1.0, 0.4, 0.9f32];
 
         let params = glium::DrawParameters {
             depth: glium::Depth {
-                test: glium::DepthTest::IfLess,
+                test: glium::draw_parameters::DepthTest::IfLess,
                 write: true,
-                ..Default::default()
+                .. Default::default()
             },
-            backface_culling: glium::draw_parameters::BackfaceCullingMode::CullClockwise,
-            ..Default::default()
+            .. Default::default()
         };
 
-        let view = view_matrix(&[2.0, -1.0, 1.0], &[-2.0, 1.0, 1.0], &[0.0, 1.0, 0.0], zoom);
-
-        target
-            .draw(
-                (&positions, &normals),
-                &indices,
-                &program,
-                &uniform! { model: model, view: view, perspective: perspective, u_light: light },
-                &params,
-            )
-            .unwrap();
+        target.draw((&positions, &normals), &indices, &program,
+                    &uniform! { matrix: matrix, perspective: perspective, u_light: light },
+                    &params).unwrap();
         target.finish().unwrap();
     });
-}
-
-fn view_matrix(position: &[f32; 3], direction: &[f32; 3], up: &[f32; 3], zoom_factor: f32) -> [[f32; 4]; 4] {
-    let f = {
-        let f = direction;
-        let len = f[0] * f[0] + f[1] * f[1] + f[2] * f[2];
-        let len = len.sqrt();
-        [f[0] / len, f[1] / len, f[2] / len]
-    };
-
-    // Calculate the normalized side vector (right)
-    let s = [
-        up[1] * f[2] - up[2] * f[1],
-        up[2] * f[0] - up[0] * f[2],
-        up[0] * f[1] - up[1] * f[0],
-    ];
-
-    // Calculate the normalized up vector
-    let s_norm = {
-        let len = s[0] * s[0] + s[1] * s[1] + s[2] * s[2];
-        let len = len.sqrt();
-        [s[0] / len, s[1] / len, s[2] / len]
-    };
-
-    // Calculate the final up vector
-    let u = [
-        f[1] * s_norm[2] - f[2] * s_norm[1],
-        f[2] * s_norm[0] - f[0] * s_norm[2],
-        f[0] * s_norm[1] - f[1] * s_norm[0],
-    ];
-
-    // Calculate the final position with zoom applied
-    let p = [
-        position[0] + f[0] * zoom_factor,
-        position[1] + f[1] * zoom_factor,
-        position[2] + f[2] * zoom_factor,
-    ];
-
-    // Construct the view matrix
-    [
-        [s_norm[0], u[0], -f[0], 0.0],
-        [s_norm[1], u[1], -f[1], 0.0],
-        [s_norm[2], u[2], -f[2], 0.0],
-        [-s_norm[0] * p[0] - s_norm[1] * p[1] - s_norm[2] * p[2],
-         -u[0] * p[0] - u[1] * p[1] - u[2] * p[2],
-         f[0] * p[0] + f[1] * p[1] + f[2] * p[2], 1.0],
-    ]
 }
