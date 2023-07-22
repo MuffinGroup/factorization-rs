a7bea56e17cf32b9090e0b7a8c727ba255f1447b
thepigcat <benpospo@gmail.com>
2023-07-22 23:29:16 +0200

    added 2d rendering and update dependencies

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,13 @@ extern crate glium;
 mod teapot;
 #[macro_use]
 mod logger;
-mod util;
 mod colors;
+mod shapes;
+mod util;
 
+use shapes::get_shape;
 use util::*;
- 
+
 fn main() {
     #[allow(unused_imports)]
     use glium::{glutin, Surface};
@@ -30,33 +32,45 @@ fn main() {
 
     let mut zoom = 10.0;
 
-    let vertex_shader_3d_src = include_str!("../resources/shaders/vertex_shader_3d.vert");
-    let vertex_shader_3d = &load_shader(vertex_shader_3d_src, "vertex_shader.vert");
+    let square_shape = get_shape();
 
-    let fragment_shader_3d_src = include_str!("../resources/shaders/fragment_shader_3d.frag");
-    let fragment_shader_3d = &load_shader(fragment_shader_3d_src, "fragment_shader.frag");
-    
-    let vertex_shader_color_src = include_str!("../resources/shaders/vertex_shader_color.vert");
-    let vertex_shader_color = &load_shader(vertex_shader_color_src, "vertex_shader.vert");
-
-    let fragment_shader_color_src = include_str!("../resources/shaders/fragment_shader_texture.frag");
-    let fragment_shader_color = &load_shader(fragment_shader_color_src, "fragment_shader.frag");
-
-    let vertex_shader_texture_src = include_str!("../resources/shaders/vertex_shader_color.vert");
-    let vertex_shader_texture = &load_shader(vertex_shader_texture_src, "vertex_shader.vert");
-
-    let fragment_shader_texture_src = include_str!("../resources/shaders/fragment_shader_texture.frag");
-    let fragment_shader_texture = &load_shader(fragment_shader_texture_src, "fragment_shader.frag");
+    let mut vertex_buffer = glium::VertexBuffer::new(&display, &square_shape).unwrap();
+    let indices_2d = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
 
     let example_image_src = include_bytes!("../resources/textures/test.png");
-    let example_image = &load_image(example_image_src, "test.png", &display);
+    let example_image = load_image(example_image_src, "test.png", &display);
 
-    let program_3d =
-        glium::Program::from_source(&display, &vertex_shader_3d, &fragment_shader_3d, None)
+    let vertex_shader_src = include_str!("../resources/shaders/vertex_shader_3d.vert");
+    let vertex_shader = load_shader(vertex_shader_src, "vertex_shader.vert");
+
+    let fragment_shader_src = include_str!("../resources/shaders/fragment_shader_3d.frag");
+    let fragment_shader = load_shader(fragment_shader_src, "fragment_shader_3d.frag");
+
+    let vertex_shader_color_src = include_str!("../resources/shaders/vertex_shader_color.vert");
+    let vertex_shader_color = load_shader(vertex_shader_color_src, "vertex_shader_color.vert");
+
+    let fragment_shader_color_src = include_str!("../resources/shaders/fragment_shader_color.frag");
+    let fragment_shader_color =
+        load_shader(fragment_shader_color_src, "fragment_shader_color.frag");
+
+    let vertex_shader_texture_src = include_str!("../resources/shaders/vertex_shader_texture.vert");
+    let vertex_shader_texture =
+        load_shader(vertex_shader_texture_src, "vertex_shader_texture.vert");
+
+    let fragment_shader_texture_src =
+        include_str!("../resources/shaders/fragment_shader_texture.frag");
+    let fragment_shader_texture =
+        load_shader(fragment_shader_texture_src, "fragment_shader_texture.frag");
+
+    let program =
+        glium::Program::from_source(&display, &vertex_shader, &fragment_shader, None).unwrap();
+
+    let program_color =
+        glium::Program::from_source(&display, &vertex_shader_color, &fragment_shader_color, None)
             .unwrap();
 
-    let program_2d_color =
-        glium::Program::from_source(&display, &vertex_shader_color, &fragment_shader_color, None)
+    let program_texture =
+        glium::Program::from_source(&display, &vertex_shader_texture, &fragment_shader_texture, None)
             .unwrap();
 
     log!("Started sucessful");
@@ -97,6 +111,24 @@ fn main() {
 
         let mut target = display.draw();
         target.clear_color_and_depth((0.0, 0.0, 1.0, 1.0), 1.0);
+        let uniforms = uniform! {
+            matrix: [
+                [0.5, 0.0, 0.0, 0.0],
+                [0.0, 0.5, 0.0, 0.0],
+                [0.0, 0.0, 0.5, 0.0],
+                [0.5, 0.0, 0.0, 1.0f32],
+            ]
+        };
+
+        let texture_uniforms = uniform! {
+            matrix: [
+                [0.5, 0.0, 0.0, 0.0],
+                [0.0, 0.5, 0.0, 0.0],
+                [0.0, 0.0, 0.5, 0.0],
+                [-0.5, 0.0, 0.0, 1.0f32],
+            ],
+            tex: &example_image
+        };
 
         let model = [
             [0.01, 0.0, 0.0, 0.0],
@@ -140,16 +172,40 @@ fn main() {
             .draw(
                 (&positions, &normals),
                 &indices,
-                &program_3d,
+                &program,
                 &uniform! { model: model, view: view, perspective: perspective, u_light: light },
                 &params,
+            )
+            .unwrap();
+        target
+            .draw(
+                &vertex_buffer,
+                &indices_2d,
+                &program_color,
+                &uniforms,
+                &Default::default(),
+            )
+            .unwrap();
+
+        target
+            .draw(
+                &vertex_buffer,
+                &indices_2d,
+                &program_texture,
+                &texture_uniforms,
+                &Default::default(),
             )
             .unwrap();
         target.finish().unwrap();
     });
 }
 
-fn view_matrix(position: &[f32; 3], direction: &[f32; 3], up: &[f32; 3], zoom_factor: f32) -> [[f32; 4]; 4] {
+fn view_matrix(
+    position: &[f32; 3],
+    direction: &[f32; 3],
+    up: &[f32; 3],
+    zoom_factor: f32,
+) -> [[f32; 4]; 4] {
     let f = {
         let f = direction;
         let len = f[0] * f[0] + f[1] * f[1] + f[2] * f[2];
@@ -190,8 +246,11 @@ fn view_matrix(position: &[f32; 3], direction: &[f32; 3], up: &[f32; 3], zoom_fa
         [s_norm[0], u[0], -f[0], 0.0],
         [s_norm[1], u[1], -f[1], 0.0],
         [s_norm[2], u[2], -f[2], 0.0],
-        [-s_norm[0] * p[0] - s_norm[1] * p[1] - s_norm[2] * p[2],
-         -u[0] * p[0] - u[1] * p[1] - u[2] * p[2],
-         f[0] * p[0] + f[1] * p[1] + f[2] * p[2], 1.0],
+        [
+            -s_norm[0] * p[0] - s_norm[1] * p[1] - s_norm[2] * p[2],
+            -u[0] * p[0] - u[1] * p[1] - u[2] * p[2],
+            f[0] * p[0] + f[1] * p[1] + f[2] * p[2],
+            1.0,
+        ],
     ]
 }
---
+++ src/shapes.rs
@@ -0,0 +1,74 @@
+#[derive(Clone, Copy)]
+pub struct Vertex {
+    position: [f32; 2],
+    tex_coords: [f32; 2],
+    rgb: [f32; 3],
+}
+
+implement_vertex!(Vertex, position, tex_coords, rgb);
+
+pub fn get_shape() -> Vec<Vertex> {
+    let square_vertex1 = Vertex {
+        position: [-0.5, -0.5],
+        tex_coords: [0.0, 0.0],
+        rgb: [0.0, 0.0, 0.0],
+    };
+    let square_vertex2 = Vertex {
+        position: [-0.5, 0.5],
+        tex_coords: [0.0, 0.0],
+        rgb: [0.0, 0.0, 0.0],
+    };
+    let square_vertex3 = Vertex {
+        position: [0.5, -0.5],
+        tex_coords: [0.0, 0.0],
+        rgb: [1.0, 1.0, 1.0],
+    };
+    let square_vertex4 = Vertex {
+        position: [0.5, -0.5],
+        tex_coords: [0.0, 0.0],
+        rgb: [1.0, 1.0, 1.0],
+    };
+    let square_vertex5 = Vertex {
+        position: [-0.5, 0.5],
+        tex_coords: [0.0, 0.0],
+        rgb: [0.0, 0.0, 0.0],
+    };
+    let square_vertex6 = Vertex {
+        position: [0.5, 0.5],
+        tex_coords: [0.0, 0.0],
+        rgb: [1.0, 1.0, 1.0],
+    };
+
+    let vertex1 = Vertex {
+        position: [-0.5, -0.5],
+        tex_coords: [0.0, 0.0],
+        rgb: [1.0, 0.0, 1.0],
+    };
+    let vertex2 = Vertex {
+        position: [0.0, 0.5],
+        tex_coords: [0.0, 1.0],
+        rgb: [0.0, 1.0, 1.0],
+    };
+    let vertex3 = Vertex {
+        position: [0.5, -0.25],
+        tex_coords: [1.0, 0.0],
+        rgb: [1.0, 1.0, 1.0],
+    };
+
+    let square_shape = vec![
+        square_vertex1,
+        square_vertex2,
+        square_vertex3,
+        square_vertex4,
+        square_vertex5,
+        square_vertex6,
+    ];
+
+    let triangle_shape = vec![
+        vertex1,
+        vertex2,
+        vertex3
+    ];
+    // square_shape // returns the square shape
+    triangle_shape
+}
