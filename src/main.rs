c31f55ebdea2babc0b6493981f21e05ae41e3e32
thepigcat <benpospo@gmail.com>
2023-07-24 16:29:19 +0200

    start working on updating code base

--- src/main.rs
+++ src/main.rs
@@ -80,9 +80,6 @@ fn main() {
     log!("Started sucessful");
 
     event_loop.run(move |event, _, control_flow| {
-        let next_frame_time =
-            std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
-        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
 
         match event {
             glutin::event::Event::WindowEvent { event, .. } => match event {
@@ -104,13 +101,8 @@ fn main() {
                     }
                 }
                 _ => return,
-            },
-            glutin::event::Event::NewEvents(cause) => match cause {
-                glutin::event::StartCause::ResumeTimeReached { .. } => (),
-                glutin::event::StartCause::Init => (),
-                _ => return,
-            },
-            _ => return,
+            }
+            _ => {}
         }
 
         let mut target = display.draw();
