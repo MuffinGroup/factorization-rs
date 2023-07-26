5b16cecc285ec61b7e733fc3e3f8efe1abb66b75
thepigcat <benpospo@gmail.com>
2023-07-26 22:30:55 +0200

    set up window

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+use winit::{event::*, event_loop::EventLoop, window::WindowBuilder};
+
+pub fn run() {
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == window.id() => match event {
+            WindowEvent::CloseRequested
+            | WindowEvent::KeyboardInput {
+                input:
+                    KeyboardInput {
+                        state: ElementState::Pressed,
+                        virtual_keycode: Some(VirtualKeyCode::Escape),
+                        ..
+                    },
+                ..
+            } => control_flow.set_exit(),
+            _ => {}
+        },
+        _ => {}
+    });
+}
