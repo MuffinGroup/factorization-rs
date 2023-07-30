706e37db321eb1cc40ac8f2be842d39cf965f974
thepigcat <benpospo@gmail.com>
2023-07-30 21:53:11 +0200

    multiple pipelines

--- src/state.rs
+++ src/state.rs
@@ -10,6 +10,7 @@ pub struct State {
     pub window: Window,
 
     pub render_pipeline: wgpu::RenderPipeline,
+    pub render_pipeline_challenge: wgpu::RenderPipeline
 }
 
 impl State {
@@ -125,6 +126,46 @@ impl State {
             multiview: None,
         });
 
+        let shader = device.create_shader_module(wgpu::include_wgsl!("shader_challenge.wgsl"));
+        
+        let render_pipeline_challenge = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: "vs_main",
+                buffers: &[],
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &shader,
+                entry_point: "fs_main",
+                targets: &[Some(wgpu::ColorTargetState {
+                    format: config.format,
+                    blend: Some(wgpu::BlendState::REPLACE),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList, // 1.
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw, // 2.
+                cull_mode: Some(wgpu::Face::Back),
+                // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
+                polygon_mode: wgpu::PolygonMode::Fill,
+                // Requires Features::DEPTH_CLIP_CONTROL
+                unclipped_depth: false,
+                // Requires Features::CONSERVATIVE_RASTERIZATION
+                conservative: false,
+            },
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState {
+                count: 1,
+                mask: !0,
+                alpha_to_coverage_enabled: false,
+            },
+            multiview: None,
+        });
+
         Self {
             window,
             surface,
@@ -133,6 +174,7 @@ impl State {
             config,
             size,
             render_pipeline,
+            render_pipeline_challenge
         }
     }
 
@@ -188,6 +230,8 @@ impl State {
             });
 
             render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.draw(0..3, 0..1);
+            render_pass.set_pipeline(&self.render_pipeline_challenge);
             render_pass.draw(0..3, 0..1)
         }
         self.queue.submit(std::iter::once(encoder.finish()));
